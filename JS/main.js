d16b4ead78dfd97604138b5ad7074162a96c79ff
MustafaHamdy10 <mustafahamdy1010@gmail.com>
2024-12-06 17:31:13 +0200

    first commit

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+
+let image=document.getElementById("image");
+let container=document.querySelector(".container");
+
+function changeImage(img){
+    image.src=img;
+}
+
+function changeBackground(color){
+    container.style.background=color;
+}
